309b327d1209e0d5f6e3e2a6e119f82ad7d8e338
democzar <demo.czar@gmail.com>
2014-08-25 00:46:25 +0530

    R code for cached inverse matrix function set

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Comments are provided in respective lines describing their purpose in brief
 
-## Write a short comment describing this function
+## makeCacheMatrix() creates a "matrix" object that can
+## cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {             # x will be a matrix
+    mi <- NULL             # will store the inverse of the matrix and reset to 
+                           # NULL whenever makeCacheMatrix() function is called
 
+    get <- function() x                     # gets the original matrix value
+    
+    setminverse <- function(solve) mi <<- solve  # called by cacheSolve()
+                                             # will super assign to store value
+
+    getminverse <- function() mi         # On subsequent access of same matrix,
+    # the cached inverse matrix is returned
+    
+    list(get = get,                     # Lists all the functions that are part
+         setminverse = setminverse,     # of the object. This returns along
+         getminverse = getminverse)     # with the newly created object
 }
 
+## cacheSolve() computes the inverse of the "matrix" returned by
+## makeCacheMatrix(). For a pre-calculated and unchanged "matrix" obect,
+## function cacheSolve() retrieves the inverse from the cache
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...) {    # object created by 1st function serves as
+                                    # input for this function
+    mi <- x$getminverse()            # accesses the object by the list method
+    
+    if(!is.null(mi)) {              # check if inverse is already available
+        message("getting cached value")  # text confirming availability
+        return(mi)                  # returns the matrix inverse and exits
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    data <- x$get()                 # if inverse is not available, then goes
+                                    # back to 1st function to get data
+    mi <- solve(data, ...)          # calculates the inverse here and
+    x$setminverse(mi)               # stores the calculated inverse in x
+    mi                              # and returns the inverse of the matrix
 }
